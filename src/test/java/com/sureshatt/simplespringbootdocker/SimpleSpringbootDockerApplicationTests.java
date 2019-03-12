0a09f8eefc49fb3831b9073c040637f601d6168f
Attanayake Suresh (CI/ISI2) <Suresh.Attanayake@de.bosch.com>
2019-03-12 10:31:19 +0100

    Initial commit

---
+++ src/test/java/com/sureshatt/simplespringbootdocker/SimpleSpringbootDockerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sureshatt.simplespringbootdocker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleSpringbootDockerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
